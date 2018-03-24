a9b3369f3a4af7fb85c19b81698f9b9d7958f43a
Joshua <Joshua@Admins-MacBook.local>
2018-03-24 18:22:26 -0500

    changed header method

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,40 @@
 import React from 'react';
-
-class Header extends React.Component{
-    render() {
-        return (
-            <header className='top'>
-                <h1>Catch 
-                    <span className="ofThe">
-                        <span className="of">of</span> 
-                        <span className="the">the</span> 
-                    </span>
-                    Day
-                </h1>
-                <h3 className="tagline">
-                    <span>Fresh Seafood Market</span>
-                </h3>
-            </header>
+//stateless funtional component with
+// implicit return
+const Header = props =>(
+    <header className='top'>
+        <h1>Catch 
+            <span className="ofThe">
+                <span className="of">of</span> 
+                <span className="the">the</span> 
+            </span>
+            Day
+        </h1>
+        <h3 className="tagline">
+            <span>{props.tagline}</span>
+        </h3>
+    </header>
+    
+);
+// same as above
+// class Header extends React.Component{
+//     render() {
+//         return (
+//             <header className='top'>
+//                 <h1>Catch 
+//                     <span className="ofThe">
+//                         <span className="of">of</span> 
+//                         <span className="the">the</span> 
+//                     </span>
+//                     Day
+//                 </h1>
+//                 <h3 className="tagline">
+//                     <span>{this.props.tagline}</span>
+//                 </h3>
+//             </header>
             
-        );
-    }
-}
+//         );
+//     }
+// }
 
 export default Header;
