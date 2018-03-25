c64d9d5671e957383a273e88228d771f9d25c4ea
Joshua <Joshua@Admins-MacBook.local>
2018-03-24 22:11:10 -0500

    created add fish component and add pass to state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,21 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component{
+    //use property to set state so we can pass the add fish form data to all components
+    state = {
+        //set state as value type it will be ie string, object...
+        fishes:{},
+        order:{}
+    };
+    addFish = fish => {
+        console.log('Adding a fish!');
+        //take a copy of exiting state
+        const fishes = {...this.state.fishes};
+        //add new fish to fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        //set the new fishes object to state
+        this.setState({fishes})
+    };
     render(){
 
         return (
@@ -12,7 +27,7 @@ class App extends React.Component{
                     <Header/>
                 </div>
                 <Order/>
-                <Inventory/>
+                <Inventory addFish={this.addFish}/>
                 
             </div>
         );
