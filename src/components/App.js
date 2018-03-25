d6e8f0f40ecb918a96b894b395ce2836a5c91dd9
Joshua <Joshua@Admins-MacBook.local>
2018-03-24 22:22:42 -0500

    add load sample fishes button to load predefined sample fishes into state for showing examples and having data to display

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
-
+import sampleFishes from '../sample-fishes'
 class App extends React.Component{
     //use property to set state so we can pass the add fish form data to all components
     state = {
@@ -17,7 +17,14 @@ class App extends React.Component{
         //add new fish to fishes variable
         fishes[`fish${Date.now()}`] = fish;
         //set the new fishes object to state
+        //if obj names are the same can abbrev with just one name
         this.setState({fishes})
+    };
+
+    loadSampleFishes = ()=>{
+        //set the new fishes object to state
+        //if obj names are diff      
+        this.setState({fishes:sampleFishes});
     };
     render(){
 
@@ -27,7 +34,7 @@ class App extends React.Component{
                     <Header/>
                 </div>
                 <Order/>
-                <Inventory addFish={this.addFish}/>
+                <Inventory addFish={this.addFish} loadSampleFishes = {this.loadSampleFishes}/>
                 
             </div>
         );
